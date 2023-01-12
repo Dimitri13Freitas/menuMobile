3552f06ee8a6ebbfd1c7fff7df54b59cac5d81ca
Dimitri Freitas <freitasdimitri7@gmail.com>
2023-01-12 15:04:59 -0300

    Animation

--- js/DragAndDrop.js
+++ js/DragAndDrop.js
@@ -21,11 +21,15 @@ export class DragAndDrop {
     this.wrapper.addEventListener('mouseup',this.endEvent);
     this.wrapper.addEventListener('mouseleave',this.mouseLeave);
   }
+  transition(active) {
+    this.wrapper.style.transition = active ? '.3s' : '';
+  }
   startEvent(e) {
     e.preventDefault()
     this.values.clickPosition = e.layerX;
     e.preventDefault();
     this.wrapper.addEventListener('mousemove',this.midleEvent);
+    this.transition(false);
   }
   midleEvent(e) {
     this.values.position = e.clientX - this.values.clickPosition;
@@ -33,10 +37,13 @@ export class DragAndDrop {
     if(e.clientX > this.values.clickPosition) this.wrapper.style.left = `${0}px`;
     if(this.values.position < this.navSize) this.wrapper.style.left = `${this.navSize - 1}px`;
   }
-  endEvent(e) {
+  endEvent() {
+    this.transition(true);
     this.wrapper.removeEventListener('mousemove',this.midleEvent);
+    if(this.values.position < (this.navSize / 2)) this.wrapper.style.left = `${this.navSize - 1}px`;
+    if(this.values.position > (this.navSize / 2)) this.wrapper.style.left = `${0}px`;
   }
   mouseLeave(e) {
-    this.wrapper.removeEventListener('mousemove', this.midleEvent)
+    this.wrapper.removeEventListener('mousemove',this.midleEvent);
   }
 }
--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 import { DragAndDrop } from '../js/DragAndDrop.js';
 
 const seila = new DragAndDrop('#wrapper nav','#wrapper');
+
 seila.init()
