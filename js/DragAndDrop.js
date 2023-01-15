6cd9ff575d05ca7e163a3f10f19d845c9fa7f401
Dimitri Freitas <freitasdimitri7@gmail.com>
2023-01-14 23:04:04 -0300

    Mobile Suport

--- js/DragAndDrop.js
+++ js/DragAndDrop.js
@@ -1,10 +1,9 @@
 export class DragAndDrop {
-  constructor(nav, wrapper, btnMenu){
+  constructor(nav, wrapper){
     this.wrapper = document.querySelector(wrapper);
     this.nav = document.querySelector(nav);
     this.navSize = -this.nav.getBoundingClientRect().width;
     this.values = {clickPosition: 0, position: 0,};
-    this.btnMenu = btnMenu ? document.querySelector(btnMenu) : null;
   }
   init() {
     this.wrapper.style.left = `${this.navSize - 1}px`;
@@ -15,12 +14,10 @@ export class DragAndDrop {
     this.startEvent = this.startEvent.bind(this);
     this.midleEvent = this.midleEvent.bind(this);
     this.endEvent = this.endEvent.bind(this);
-    this.mouseLeave = this.mouseLeave.bind(this);
   }
   addEvents() {
     this.wrapper.addEventListener('mousedown',this.startEvent);
-    this.wrapper.addEventListener('mouseup',this.endEvent);
-    this.wrapper.addEventListener('mouseleave',this.mouseLeave);
+    this.wrapper.addEventListener('touchstart',this.startEvent);
   }
   transition(active) {
     this.wrapper.style.transition = active ? '.3s' : '';
@@ -29,16 +26,20 @@ export class DragAndDrop {
     if(active) {
       document.documentElement.addEventListener('mousemove',this.midleEvent);
       document.documentElement.addEventListener('mouseup',this.endEvent);
+
+      document.documentElement.addEventListener('touchmove',this.midleEvent);
+      document.documentElement.addEventListener('touchend',this.endEvent);
     } else {
       document.documentElement.removeEventListener('mousemove',this.midleEvent);
       document.documentElement.removeEventListener('mouseup',this.endEvent);
+
+      document.documentElement.removeEventListener('touchmove',this.midleEvent);
+      document.documentElement.removeEventListener('touchend',this.endEvent);
     }
   }
   startEvent(e) {
-    e.preventDefault()
-    this.values.clickPosition = e.layerX;
     e.preventDefault();
-    this.wrapper.addEventListener('mousemove',this.midleEvent);
+    this.values.clickPosition = e.layerX;
     this.transition(false);
     this.teste(true);
   }
@@ -48,20 +49,14 @@ export class DragAndDrop {
     if(e.clientX > this.values.clickPosition) this.wrapper.style.left = `${0}px`;
     if(this.values.position < this.navSize) this.wrapper.style.left = `${this.navSize - 1}px`;
   }
-  endEvent() {
-    console.log(this.values.position)
-    console.log(this.navSize)
+  endEvent(e) {
     this.teste(false);
     this.transition(true);
-    this.wrapper.removeEventListener('mousemove',this.midleEvent);
     if(this.values.position < (this.navSize / 2)) this.wrapper.style.left = `${this.navSize - 1}px`;
-    if(this.values.position > (this.navSize / 2)) {
-      // console.log(this.values.position)
-      // console.log(this.navSize / 2)
-      this.wrapper.style.left = `${0}px`;
-    };
+    if(this.values.position > (this.navSize / 2)) this.wrapper.style.left = `${0}px`;
   }
-  mouseLeave(e) {
-    // this.wrapper.removeEventListener('mousemove',this.midleEvent);
+  button(e) {
+
   }
+
 }
